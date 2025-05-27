4cdc18cc774cf09d85faffa23160c4e1b8a2dc66
Negasi Gidena <negasig21@gmail.com>
2025-05-27 13:31:20 +0300

    role based access

--- kycsystem_demo/src/main/java/com/example/kycsystem_demo/Controller/Maincontroller.java
+++ kycsystem_demo/src/main/java/com/example/kycsystem_demo/Controller/Maincontroller.java
@@ -1,60 +1,73 @@
 package com.example.kycsystem_demo.Controller;
 
 import com.example.kycsystem_demo.Model.Customer;
+import com.example.kycsystem_demo.Model.UserDTO;
+import com.example.kycsystem_demo.Ratelimitservice;
 import com.example.kycsystem_demo.Repository.CustomerRepository;
-import io.jsonwebtoken.Jwts;
+import io.github.bucket4j.Bucket;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
-import org.hibernate.query.spi.QueryInterpretationCache;
+import jakarta.validation.constraints.Positive;
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.web.bind.annotation.*;
 
 
-import java.security.Key;
+import javax.crypto.SecretKey;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+
+import static io.jsonwebtoken.Jwts.*;
 
 @RequestMapping("/Api/v1")
 @RestController
+@Data
 public class Maincontroller {
     private String jwtSecret = "negasihadusdfsjfsadfsdgfsjgfshdfsgfsgfdsgfg2526485634573563845638456358345634sh";
-    private Key getSigninkey(){
-        byte[] keybaytes= Decoders.BASE64.decode(jwtSecret);
+
+    private SecretKey getSigninkey() {
+        byte[] keybaytes = Decoders.BASE64.decode(jwtSecret);
         return Keys.hmacShaKeyFor(keybaytes);
     }
+
     @Autowired
     CustomerRepository crepository;
-    @GetMapping("/customers")
-    public Object getCustomers(){
-        List<Customer> lc=crepository.findAll();
-        if(lc.size()<1){
-            return "No records";
-        }
-        else {
-            return lc;
-        }
-    }
-    @GetMapping("/customer/{id}")
-    public Object getCustomerByid(@PathVariable int id){
+    @Autowired
+    Ratelimitservice ratelimitservice;
 
-        Optional<Customer> lc=crepository.findById(id);
-        if(lc.isEmpty()){
-            return "No record found with id  " +id;
-        }
-        else {
+    @GetMapping("/customers")
+    public Object getCustomers() {
+        List<Customer> lc = crepository.findAll();
+        if (lc.size() < 1) {
+            return "No records";
+        } else {
             return lc;
         }
     }
+
+    @GetMapping("/customer/{id}")
+
+    public ResponseEntity<UserDTO> getCustomerByid(@PathVariable @Positive int id) {
+        Customer lc =crepository.findById(id).get();
+        UserDTO userDTO=new UserDTO(lc.getFirstName(),lc.getLastName(),lc.getProfession(),lc.getAge(),lc.getGender(), lc.getEmail());
+        return ResponseEntity.ok(userDTO);
+    }
+
     @PostMapping("/addCustomer")
+
     public ResponseEntity<String> registerCustomer(@Valid @RequestBody Customer customer  ) {
         List<Customer> customer11 = crepository.findByusername(customer.getUsername());
         if (customer11.isEmpty()) {
 
             Customer customer1=new Customer();
+            String password=BCrypt.hashpw(customer.getPassword(), BCrypt.gensalt(10));
             customer1.setFirstName(customer.getFirstName());
             customer1.setLastName(customer.getLastName());
             customer1.setProfession(customer.getProfession());
@@ -63,7 +76,8 @@ public class Maincontroller {
             customer1.setSalary(customer.getSalary());
             customer1.setGender(customer.getGender());
             customer1.setUsername(customer.getUsername());
-            customer1.setPassword(customer.getPassword());
+            customer1.setPassword(password);
+            customer1.setRole(customer.getRole());
             crepository.save(customer1);
             return ResponseEntity.ok("Customer saved");
         }
@@ -74,17 +88,60 @@ public class Maincontroller {
     }
     @PostMapping("/login")
     public Object login(@RequestBody Customer customer){
+        Map<String, String> users=new HashMap<>();
         List<Customer> customer1=crepository.findByusername(customer.getUsername());
+        if (customer1.size()==1){
+            users.put("role",customer1.get(0).getRole());
+            users.put("username", customer1.get(0).getUsername());
+        }
         if(customer1.get(0).getUsername().equals(customer.getUsername())
-                && customer1.get(0).getPassword().equals(customer.getPassword())){
+                && BCrypt.checkpw(customer.getPassword(), customer1.get(0).getPassword())) {
             return Jwts.builder()
-                    .subject(customer.getUsername())
-                    .signWith(getSigninkey())
-                    .claim("name", customer1)
+                    .setSubject(customer.getUsername())
+                    .signWith(SignatureAlgorithm.HS256, jwtSecret)
+                    .setClaims(users)
                     .compact();
         }
         else {
             return "you have enterd incorrect credientials";
         }
+    }
+
+    @GetMapping("/profile/")
+    public Object getCustomerProfile(HttpServletRequest http) {
+        String autheader = http.getHeader("Authorization");
+
+        if (autheader != null && autheader.startsWith("Bearer ")) {
+            String token = autheader.substring(7);
+            Claims claims = Jwts.parserBuilder()
+                    .setSigningKey(jwtSecret)
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+
+            if (claims.get("role").equals("admin")) {
+                return "Access Granted";
+            }
+            else{
+                return "invalid token";
+            }
+        }
+        else {
+            return "Invalid token";
+        }
+    }
+    @GetMapping("/limited/{id}")
+    public Object protectDOSAttack(HttpServletRequest http, @PathVariable int id){
+        Bucket bucket=ratelimitservice.resolveBucket(http.getRemoteAddr());
+        Customer lc =crepository.findById(id).get();
+
+        UserDTO userDTO=new UserDTO(lc.getFirstName(),lc.getLastName(),lc.getProfession(),lc.getAge(),lc.getGender(), lc.getEmail());
+        if(bucket.tryConsume(1)){
+            return userDTO;
+        }
+        else {
+            return "Too many requests wait and try again";
+        }
+
     }
 }
--- kycsystem_demo/src/main/java/com/example/kycsystem_demo/Model/UserDTO.java
+++ kycsystem_demo/src/main/java/com/example/kycsystem_demo/Model/UserDTO.java
@@ -6,14 +6,25 @@ public class UserDTO {
     private String profession;
     private Integer age;
     private String gender;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    private String email;
    public UserDTO(){}
 
-    public UserDTO(String firstName, String lastName, String profession, Integer age, String gender) {
+    public UserDTO(String firstName, String lastName, String profession, Integer age, String gender, String email) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.profession = profession;
         this.age = age;
         this.gender = gender;
+        this.email=email;
     }
 
     public String getFirstName() {
