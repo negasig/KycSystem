3bf091e95c99aeb2cf0012e1b8a249e87f47e92a
Negasi Gidena <negasig21@gmail.com>
2025-05-27 10:54:21 +0300

    test

--- kycsystem_demo/src/main/java/com/example/kycsystem_demo/Controller/Maincontroller.java
+++ kycsystem_demo/src/main/java/com/example/kycsystem_demo/Controller/Maincontroller.java
@@ -1,66 +1,55 @@
 package com.example.kycsystem_demo.Controller;
 
 import com.example.kycsystem_demo.Model.Customer;
-import com.example.kycsystem_demo.Model.UserDTO;
-import com.example.kycsystem_demo.Ratelimitservice;
 import com.example.kycsystem_demo.Repository.CustomerRepository;
-import io.github.bucket4j.Bucket;
-import io.jsonwebtoken.*;
+import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.Positive;
-import lombok.Data;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.query.spi.QueryInterpretationCache;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
-import javax.crypto.SecretKey;
-import java.util.HashMap;
+import java.security.Key;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
-
-import static io.jsonwebtoken.Jwts.*;
 
 @RequestMapping("/Api/v1")
 @RestController
-@Data
 public class Maincontroller {
     private String jwtSecret = "negasihadusdfsjfsadfsdgfsjgfshdfsgfsgfdsgfg2526485634573563845638456358345634sh";
-
-    private SecretKey getSigninkey() {
-        byte[] keybaytes = Decoders.BASE64.decode(jwtSecret);
+    private Key getSigninkey(){
+        byte[] keybaytes= Decoders.BASE64.decode(jwtSecret);
         return Keys.hmacShaKeyFor(keybaytes);
     }
-
     @Autowired
     CustomerRepository crepository;
-    @Autowired
-    Ratelimitservice ratelimitservice;
-
     @GetMapping("/customers")
-    public Object getCustomers() {
-        List<Customer> lc = crepository.findAll();
-        if (lc.size() < 1) {
+    public Object getCustomers(){
+        List<Customer> lc=crepository.findAll();
+        if(lc.size()<1){
             return "No records";
-        } else {
+        }
+        else {
             return lc;
         }
     }
-
     @GetMapping("/customer/{id}")
+    public Object getCustomerByid(@PathVariable int id){
 
-    public ResponseEntity<UserDTO> getCustomerByid(@PathVariable @Positive int id) {
-            Customer lc =crepository.findById(id).get();
-            UserDTO userDTO=new UserDTO(lc.getFirstName(),lc.getLastName(),lc.getProfession(),lc.getAge(),lc.getGender());
-            return ResponseEntity.ok(userDTO);
+        Optional<Customer> lc=crepository.findById(id);
+        if(lc.isEmpty()){
+            return "No record found with id  " +id;
+        }
+        else {
+            return lc;
+        }
     }
-
     @PostMapping("/addCustomer")
-
     public ResponseEntity<String> registerCustomer(@Valid @RequestBody Customer customer  ) {
         List<Customer> customer11 = crepository.findByusername(customer.getUsername());
         if (customer11.isEmpty()) {
@@ -83,59 +72,19 @@ public class Maincontroller {
 
         }
     }
-@PostMapping("/login")
+    @PostMapping("/login")
     public Object login(@RequestBody Customer customer){
-    Map<String, String> users=new HashMap<>();
         List<Customer> customer1=crepository.findByusername(customer.getUsername());
-        if (customer1.size()==1){
-            users.put("role",customer1.get(0).getRole());
-            users.put("username", customer1.get(0).getUsername());
-        }
         if(customer1.get(0).getUsername().equals(customer.getUsername())
                 && customer1.get(0).getPassword().equals(customer.getPassword())){
             return Jwts.builder()
-                    .setSubject(customer.getUsername())
-                    .signWith(SignatureAlgorithm.HS256, jwtSecret)
-                    .setClaims(users)
+                    .subject(customer.getUsername())
+                    .signWith(getSigninkey())
+                    .claim("name", customer1)
                     .compact();
         }
         else {
             return "you have enterd incorrect credientials";
         }
-    }
-
-    @GetMapping("/profile/")
-    public Object getCustomerProfile(HttpServletRequest http) {
-        String autheader = http.getHeader("Authorization");
-
-        if (autheader != null && autheader.startsWith("Bearer ")) {
-            String token = autheader.substring(7);
-            Claims claims = Jwts.parserBuilder()
-                    .setSigningKey(jwtSecret)
-                    .build()
-                    .parseClaimsJws(token)
-                    .getBody();
-
-            if (claims.get("role").equals("admin")) {
-                return "Access Granted";
-            }
-            else{
-                return "invalid token";
-            }
-        }
-        else {
-            return "Invalid token";
-        }
-    }
-    @GetMapping("/limited/")
-    public String  protectDOSAttack(HttpServletRequest http){
-     Bucket bucket=ratelimitservice.resolveBucket(http.getRemoteAddr());
-     if(bucket.tryConsume(1)){
-         return "Request Sucessfull";
-     }
-     else {
-         return "Too many requests at a time please wait";
-     }
-
     }
 }
